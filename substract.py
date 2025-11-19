ab9d3b7441f3211cc911d5441da0775cc8725347
yonodoy <yened207@gmail.com>
2025-11-19 23:46:25 +0000

    Update substract.py

--- substract.py
+++ substract.py
@@ -3,6 +3,8 @@ def substract(num1, num2):
 	The following function substracts two numbers,
 	adding the first one the negative part of the second one
 	'''
+	if type(num1) != (int or float) or type(num2) != (int or float):
+		return None
 	num2 = -num2
 	operation = num1 + num2
 	return operation
