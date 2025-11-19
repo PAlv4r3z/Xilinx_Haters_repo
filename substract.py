cdfcb6823e31bee9a463f1ffd0950bb7919e34a3
yonodoy <yened207@gmail.com>
2025-11-19 19:31:28 +0000

    substract.py

---
+++ substract.py
@@ -0,0 +1,8 @@
+def substract(num1, num2):
+	'''
+	The following function substracts two numbers,
+	adding the first one the negative part of the second one
+	'''
+	num2 = -num2
+	operation = num1 + num2
+	return operation
