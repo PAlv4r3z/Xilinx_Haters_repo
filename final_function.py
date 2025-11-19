f78cbc665f661d3fe31d5ef321c029fe91dd6a5e
PAlv4r3z <pabloalvacab@gmail.com>
2025-11-19 19:43:28 +0000

    Update final_function.py

--- final_function.py
+++ final_function.py
@@ -8,7 +8,7 @@ def calculator(operation, num1, num2):  #Evaluates operation and performs the sp
     if operation == 3:
         return divide(num1, num2)
 
-operation = input("Type in 0 for +, 1 for -, 2 for * and 3 for /: )
+operation = input("Type in 0 for +, 1 for -, 2 for * and 3 for /:" )
 num1 = input("Insert your first number: ")
 num2 = input(("Insert your first number: ")
 print(calculator(operation, num1, num2))
