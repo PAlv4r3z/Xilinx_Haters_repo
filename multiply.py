4d2f465e4251ded99309938163bdbcbe2d4c4183
_T0m4s_ <antoniorodriguezalvarez07@gmail.com>
2025-11-20 12:52:18 +0000

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,4 +1,5 @@
 def multiply(num1, num2):
+    # The following function multiplies two numbers by adding the first one as many times as the second number.
     multiplier = 0
     if num2 > 0:
         for _ in range(num2):
